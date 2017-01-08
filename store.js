0c7d3cb9fc6caefe4c5176b5f8bda74357c06ad5
e.tarasenko <exh@xakep.ru>
2017-01-09 02:12:36 +0300

    extract statementText add statementHTML

--- store.js
+++ store.js
@@ -1,20 +1,36 @@
 "use strict"
 
-function statement(customer, movies) {
-
-	let totalFrequentRenterPoints = getTotalFrequentRenterPoints(customer);
-
-	let result = `Rental Record for ${customer.name}\n`;
-
-	for (let rental of customer.rentals) {
-		//print figures for this rental
-		result += `\t${getMovieBy(rental).title}\t${getAmount(rental)}\n`;
+function statement(customer, movies, format="html") {
+	if (format=="text") {
+		return statementText();
+	} else if(format=="html")	{
+		return statementHTML();
 	}
-	// add footer lines
-	result += `Amount owed is ${getTotalAmount(customer)}\n`;
-	result += `You earned ${totalFrequentRenterPoints} frequent renter points\n`;
 
-	return result;
+	function statementHTML()	{
+		let result = `<h1>Rental Record for <em>${customer.name}</em></h1>\n`;
+		result += "<table>\n";
+		for (let rental of customer.rentals) {
+			result += `  <tr><td>${getMovieBy(rental).title}</td><td>${getAmount(rental)}</td></tr>\n`;
+		}
+		result += "</table>\n";
+		result += `<p>Amount owed is <em>${getTotalAmount(customer)}</em></p>\n`;
+		result += `<p>You earned <em>${getTotalFrequentRenterPoints(customer)}</em> frequent renter points</p>\n`;
+		return result
+	}
+	
+	function statementText() {
+		let result = `Rental Record for ${customer.name}\n`;
+		for (let rental of customer.rentals) {
+			//print figures for this rental
+			result += `\t${getMovieBy(rental).title}\t${getAmount(rental)}\n`;
+		}
+		// add footer lines
+		result += `Amount owed is ${getTotalAmount(customer)}\n`;
+		result += `You earned ${getTotalFrequentRenterPoints(customer)} frequent renter points\n`;
+
+		return result;
+	}
 
 	function getMovieBy(rental) {
 		return movies[rental.movieID];
