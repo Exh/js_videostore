73e3b655f82c8fe41991fbee49ef0a1f2b3450e8
e.tarasenko <exh@xakep.ru>
2017-01-15 22:36:15 +0300

    Created class Rental

---
+++ Customer.js
@@ -0,0 +1,37 @@
+/**
+ * Created by tarasenko on 15.01.2017.
+ */
+
+"use strict";
+
+class Customer {
+	constructor(data) {
+		this._data = data;
+
+	}
+	get name() {
+		return this._data.name;
+	}
+
+	get rentals(){
+		return this._data.rentals.map(rental => new Rental(rental));
+	}
+
+}
+
+class Rental {
+	constructor(data) {
+		this._data = data;
+	}
+
+	get movieID() {
+		return this._data.movieID;
+	}
+
+	get days() {
+		return this._data.days;
+	}
+}
+
+
+module.exports = Customer;
